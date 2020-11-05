253127e09a0b017a129802bc09420167f8d32156
Morris Tseng <mephisto41@gmail.com>
2020-11-05 14:47:29 +0800

    test

--- src/react/hooks/useQuery.ts
+++ src/react/hooks/useQuery.ts
@@ -9,6 +9,7 @@ export function useQuery<TData = any, TVariables = OperationVariables>(
   query: DocumentNode | TypedDocumentNode<TData, TVariables>,
   options?: QueryHookOptions<TData, TVariables>
 ) {
+  console.log("useQuery")
   return useBaseQuery<TData, TVariables>(query, options, false) as QueryResult<
     TData,
     TVariables
